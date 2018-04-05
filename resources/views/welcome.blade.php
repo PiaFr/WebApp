825aac9374bb034be762f7234561a36a45ccb690
annazeller <annalena.zeller@gmail.com>
2018-04-05 10:19:08 +0200

    typo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>easyLife</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    EasyLife Calender
+                    EasyLife Calendar
                 </div>
 
                 <div class="links">
